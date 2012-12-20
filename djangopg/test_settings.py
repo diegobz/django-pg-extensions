308bb8d27954d8e6fa69b8d90aa94d1459ee5c28
Apostolos Bessas <mpessas@transifex.com>
2012-12-20 12:13:27 +0200

    Add settings for running the tests

---
+++ djangopg/test_settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
